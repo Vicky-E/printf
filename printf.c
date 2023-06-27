a946a11ed52ae991724675af5ca4b51f5c3d42be
JennieJennae <you@example.com>
2023-06-27 16:06:32 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: buffer to be printed
- * @buff_ind: Index at which to add next char, represents the length.
- */
-
 void print_buffer(char buffer[], int *buff_ind);
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
 
 /**
  * _printf - prints output according to a format
@@ -57,10 +45,21 @@ int _printf(const char *format, ...)
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
