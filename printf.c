7d827780f4172fbe6f05651d78d61e34a562439c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-06-27 10:49:42 -0700

    added blank line after declarations

--- printf.c
+++ printf.c
@@ -9,16 +9,17 @@ int _printf(const char *format, ...)
 	char ch, *str;
 	int ind, count = 0;
 	va_list args;
+
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	for (ind = 0; format[ind] != '\0';ind++)
+	for (ind = 0; format[ind] != '\0'; ind++)
 	{
 		if (format[ind] == '%' && format[ind + 1] == 'c')
 		{
 			ind++;
 			ch = va_arg(args, int);
-			write(1,&ch,1);
+			write(1, &ch, 1);
 			count++;
 		}
 		else if (format[ind] == '%' && format[ind + 1] == '%')
@@ -33,14 +34,14 @@ int _printf(const char *format, ...)
 			str = va_arg(args, char*);
 			while (*str)
 			{
-				write(1,&str,1);
+				write(1, &str, 1);
 				str++;
 				count++;
 			}
 		}
 		else
 		{
-			write(1,&format[ind],1);
+			write(1, &format[ind], 1);
 			count++;
 		}
 	}
