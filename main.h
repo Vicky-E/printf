0acbfdb7e88aab611ff79ba8e12117aadc520e8c
JennieJennae <you@example.com>
2023-06-27 17:33:23 -0700

    main.h

--- main.h
+++ main.h
@@ -6,4 +6,14 @@
 #include <stdlib.h>
 #include <string.h>
 int _printf(const char *format, ...);
+
+int (*check_specifier(const char *specifier))(va_list);
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+int print_binary(va_list args);
+int print_S(va_list args);
+
 #endif
